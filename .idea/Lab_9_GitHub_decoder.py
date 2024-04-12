5dbeb821884ae7519e66031b6af20445b3e1b717
gbw30 <166647742+gbw30@users.noreply.github.com>
2024-04-12 16:22:43 -0400

    Added decoder to encoder file

--- .idea/Lab_9_GitHub_decoder.py
+++ .idea/Lab_9_GitHub_decoder.py
@@ -1,14 +1,5 @@
-
-# Encoder Lab 9 COP3502C
-
-def encode_password(password):
-    encoded_password = ""
-    for digit in password:
-        encoded_digit = str((int(digit) + 3) % 10)
-        encoded_password += encoded_digit
-    return encoded_password
-
 # Decoder Lab 9 COP3502C
+
 def decode(password: str) -> str:
     decoded_password = ' '
     for i in password:
@@ -17,8 +8,3 @@ def decode(password: str) -> str:
             decoded = decoded + 10
         decoded_password += str(decoded)
     return decoded_password
-
-
-
-
-
